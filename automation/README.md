a9e9e6f95ba94c6455d557a47109682aa87f05d8
Paweł Nowosielski <pawel.nowosielski@golem.network>
2021-07-23 21:55:30 +0200

    M10: add repositories

---
+++ automation/README.md
@@ -0,0 +1,6 @@
+# Pliki dla Modułu 10
+
+Moduł 10 to duza sprawa. Poniewaz tworzymy co najmniej 2 repozytoria w CodeCommit
+aby nie mieszać z repozytorium dla kursu (tym repozytorium), będą one tworzone (klonowane)
+poza repozytorium.
+
---
+++ automation/repositories/cicd/commands/deploy-repository-cicd.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+PROJECT="memes-generator"
+SHARED_STAGE="shared"
+REGION="eu-west-1"
+
+######### Params ###########
+
+COMPONENT="cicd"
+TYPE="cicd"
+STACK="repository-$TYPE"
+TEMPLATE="repository"
+PARAMETERS="repository-$TYPE"
+
+######### common part #########
+
+TEMPLATE_FILE="$COMPONENT/templates/$TEMPLATE.yaml"
+PARAM_FILE="$COMPONENT/parameters/$PARAMETERS-$SHARED_STAGE.json"
+
+deploy="aws cloudformation deploy \
+    --template-file $TEMPLATE_FILE \
+    --stack-name $PROJECT-$COMPONENT-$STACK-$SHARED_STAGE \
+    --no-fail-on-empty-changeset \
+    --parameter-overrides file://$PARAM_FILE \
+    --region $REGION \
+    --tags Project=$PROJECT SharedStage=$SHARED_STAGE Component=$COMPONENT Type=$TYPE"
+
+echo $deploy
+
+$deploy
